979cfee516bc0f21f1a84df175382bb73467da42
Brian Nelson <brian@treegecko.com>
2016-04-23 09:38:53 -0700

    Initial commit - Moving from other repo

---
+++ src/ResponseObjects/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace HydrantWiki.Mobile.Api.ResponseObjects
+{ 
+    public class BaseResponse
+    {
+        public bool Success { get; set; }
+        public string Error { get; set; }
+        public string Message { get; set; }
+
+        public BaseResponse() { }
+
+        public BaseResponse(bool _success)
+        {
+            Success = _success;
+        }
+    }
+}
